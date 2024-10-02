414f42368653ee50fa4c315f192a8509643de0ad
lol <reva.ivan2012@gmail.com>
2024-10-02 13:42:51 +0300

    first commit

---
+++ config.php
@@ -0,0 +1,10 @@
+<?php
+
+defined('VG_ACCESS') or die('Access dined');
+
+const SITE_URL = 'http://myfr';
+const PATH = '/';
+const HOST = 'localhost';
+const USER = 'root';
+const PASS = '';
+const DB_NAME = 'myfr';
